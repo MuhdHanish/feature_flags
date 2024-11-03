5659ad3eab51a938b8cca2b86710c5d0b1250a48
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-03 15:07:24 +0530

    feat: added the option to fetch and show features in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+
+type FeatureFlags = {
+  newUI: boolean;
+  betaFeatures: boolean;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [features, setFeatures] = useState<FeatureFlags | null>(null);
+
+  useEffect(() => {
+    const fetchFeatureFlags = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/feature-flags/${import.meta.env.VITE_APP_ENV}/${import.meta.env.VITE_APP_PROJECT}`);
+        if (!response.ok) {
+          throw new Error(`Network response was not ok status: ${response.status}`);
+        }
+        const { flags }: { flags: FeatureFlags } = await response.json();
+        setFeatures(flags);
+      } catch (error) {
+        console.error('Error fetching feature flags:', error);
+      }
+    };
+
+    fetchFeatureFlags();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {features ? (
+        <div>
+          <h1>Feature Flags</h1>
+          <p>New UI: {features?.newUI ? 'Enabled' : 'Disabled'}</p>
+          <p>Beta Features: {features?.betaFeatures ? 'Enabled' : 'Disabled'}</p>
+        </div>
+      ) : (
+        <p>Loading feature flags...</p>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
